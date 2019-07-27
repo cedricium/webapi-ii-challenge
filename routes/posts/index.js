a4e0741cb3007a7410b8543102895faf7705e985
Cedricium <amaya.cedric@gmail.com>
2019-07-26 18:37:04 -0700

    Implement POST /api/posts route
    Used to create a new post.

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -16,4 +16,27 @@ postsRouter.get('/', async (req, res) => {
   }
 })
 
+postsRouter.post('/', async (req, res) => {
+  const { title, contents } = req.body
+  if (!title || !contents ) {
+    return res.status(400).json({
+      success: false,
+      error: `Please provide title and contents for the post`
+    })
+  }
+  try {
+    const { id } = await db.insert({ title, contents })
+    const post = await db.findById(id)
+    res.status(201).json({
+      success: true,
+      post
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: `An error occurred while saving the post to the database`
+    })
+  }
+})
+
 module.exports = postsRouter
